67993539e915623e8235ae6b894bb198dc606491
Burak Pala <74012242+burakpla@users.noreply.github.com>
2021-12-15 14:37:06 +0300

    finished

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+        mongoURI: "mongodb+srv://burakpala:x1PSFuSnXKouakpx@cluster0.xlse1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+        secret: 'yoursecret'
+    };
